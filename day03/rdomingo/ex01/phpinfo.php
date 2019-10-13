4bf70fa39b57818c605788173dbb5cdcc3652094
maurice <mauricegumbi24@gmail.com>
2019-10-13 13:52:29 +0200

    the third day

---
+++ day03/rdomingo/ex01/phpinfo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>phpinfo()</title>
+	<meta charset="utf-8" /> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">	
+	</style>
+</head>
+<body>
+	<?php
+		phpinfo(); // The <meta> tag provides metadata about the HTML document.
+	?>
+</body>
+</html>
